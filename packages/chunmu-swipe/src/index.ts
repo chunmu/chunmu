c1cb189c467b873ec04de1ac8dfda945b3b1fa96
chunmu.zhang <chunmu.zhang@dji.com>
2021-02-24 17:35:14 +0800

    fix: 修复d.ts

--- packages/chunmu-swipe/src/index.ts
+++ packages/chunmu-swipe/src/index.ts
@@ -1,7 +1,12 @@
 import Swipe from './Swipe';
+import AutoPlaySwipe from './AutoPlay';
 
 export * from './Swipe';
 export * from './AutoPlay';
 export * from './constants';
 
 export default Swipe;
+
+export {
+  AutoPlaySwipe
+}
